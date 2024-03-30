98fbc6f94ca1e1508b64e5243b56c26bce5820f2
AbdurrohmanJCom <tuckerticket@gmail.com>
2024-03-31 03:35:08 +0500

    get_location.py | getting  location, image, video and saving it to the directory without request to TG API

---
+++ get_location.py
@@ -0,0 +1,57 @@
+import telebot
+from telebot import types
+import os
+
+bot = telebot.TeleBot("6956163861:AAHiedP7PYOWS-QHeLSqyhGtJsm5aSkFrE8")
+
+@bot.message_handler(commands=['start'])
+def send_location_button(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_location = types.KeyboardButton(text="Отправить геолокацию", request_location=True)
+    keyboard.add(button_location)
+    bot.send_message(message.chat.id, "Нажмите кнопку, чтобы отправить геолокацию", reply_markup=keyboard)
+
+@bot.message_handler(content_types=['location'])
+def handle_location(message):
+    latitude = message.location.latitude
+    longitude = message.location.longitude
+    bot.send_message(message.chat.id, f"Получена геолокация: Широта {latitude}, Долгота {longitude}")
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    file_path = file_info.file_path
+    downloaded_file = bot.download_file(file_path)
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    image_path = os.path.join(current_dir, 'photo.jpg')
+    with open(image_path, 'wb') as photo:
+        photo.write(downloaded_file)
+    
+    # Отправляем картинку обратно в чат
+    with open(image_path, 'rb') as photo:
+        bot.send_photo(message.chat.id, photo)
+
+import requests
+
+# L93TD8xFcWoJNkQ1wP7650oJc55SoqqM9bfUQaRWcY4
+@bot.message_handler(content_types=['video'])
+def handle_video(message):
+    # Получаем информацию о видео
+    file_id = message.video.file_id
+    file_info = bot.get_file(file_id)
+    file_path = file_info.file_path
+    # Скачиваем видео
+    downloaded_file = bot.download_file(file_path)
+    
+    # Получаем путь к текущей директории
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Сохраняем видео в директорию с кодом бота
+    video_path = os.path.join(current_dir, 'video.mp4')
+    with open(video_path, 'wb') as video:
+        video.write(downloaded_file)
+
+
+bot.polling()
